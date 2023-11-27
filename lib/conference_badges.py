fca5b803f7edf4c59cef08e7aa1b44cc688da96b
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-27 13:35:02 +0300

    cmplt lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,20 @@
+# Function to create a badge message for a speaker
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(speakers):
+    return [badge_maker(speaker) for speaker in speakers]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    return [f"Hello, {speaker}! You'll be assigned to room {index + 1}!" for index, speaker in enumerate(speakers)]
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    room_assignments = assign_rooms(speakers)
+
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+
